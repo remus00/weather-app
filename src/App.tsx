832a098a6975a7bb96e735f07ff161a3dcac6f5d
remus-wolico <135813551+remus00@users.noreply.github.com>
2025-01-04 16:37:46 +0100

    initialize shadcn ui + prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-    return <div className="">Hello</div>;
+    return (
+        <div className="flex h-screen w-full items-center justify-center">
+            <Button>Hello world</Button>
+        </div>
+    );
 }
 
 export default App;
