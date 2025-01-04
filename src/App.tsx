6e941fb025c82711b4cfdebc8d742bdce1c8099d
remus-wolico <135813551+remus00@users.noreply.github.com>
2025-01-04 17:39:55 +0100

    add api connection and endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 import { Layout } from './components/layout';
 import { ThemeProvider } from './context/theme-provider';
 import { CityPage } from './pages/city-page';
 import { WeatherDashboard } from './pages/weather-dashboard';
 
 function App() {
+    const queryClient = new QueryClient();
+
     return (
-        <BrowserRouter>
-            <ThemeProvider defaultTheme="dark">
-                <Layout>
-                    <Routes>
-                        <Route path="/" element={<WeatherDashboard />} />
-                        <Route path="/city/:cityName" element={<CityPage />} />
-                    </Routes>
-                </Layout>
-            </ThemeProvider>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <ThemeProvider defaultTheme="dark">
+                    <Layout>
+                        <Routes>
+                            <Route path="/" element={<WeatherDashboard />} />
+                            <Route path="/city/:cityName" element={<CityPage />} />
+                        </Routes>
+                    </Layout>
+                </ThemeProvider>
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     );
 }
 
